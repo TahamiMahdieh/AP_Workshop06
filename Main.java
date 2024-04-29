b225ccf9469e1136ae899b8375df2efc9c3a0465
Mahdieh <mahdiehtahami21@gmail.com>
2024-04-29 04:15:36 -0700

    main class is defined

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Animal> animals = new ArrayList<>();
+        animals.add(new Parrot("Brazilian parrot", 5, "can speak", 50));
+        animals.add(new Parrot("green parrot", 8, "eats nuts", 40));
+        animals.add(new Eagle("Asian eagle", 8, "eagle vision", 800));
+        animals.add(new Cheetah("cheetah", 12, "fastest animal", 100));
+        animals.add(new Giraffe("giraffe", 3, "tallest animal", 30));
 
-        System.out.println("Hello world!");
+        for (Animal a : animals)
+            a.show();
+        Cheetah c = new Cheetah("cheeta", 7, "fastest", 100);
+        c.hunt((Parrot)animals.get(0));
     }
 }
