f6deda1eb6b772fa51fefb888bd725aa3bc11653
unknown <MohammadReza Shafeei>
2024-04-29 13:55:53 +0330

    mamal classes defined

--- Bird.java
+++ Bird.java
@@ -1,2 +1,11 @@
 public abstract class Bird extends Animal {
+    protected int flightHeight;
+
+    public int getFlightHeight() {
+        return flightHeight;
+    }
+
+    public void setFlightHeight(int flightHeight) {
+        this.flightHeight = flightHeight;
+    }
 }
--- Mammal.java
+++ Mammal.java
@@ -1,2 +1,15 @@
 public abstract class Mammal extends Animal {
+    protected int maxRunningSpeed;
+
+    public Mammal(String name, int age, String info, int maxRunningSpeed) {
+        super();
+    }
+
+    public int getMaxRunningSpeed() {
+        return maxRunningSpeed;
+    }
+
+    public void setMaxRunningSpeed(int maxRunningSpeed) {
+        this.maxRunningSpeed = maxRunningSpeed;
+    }
 }
